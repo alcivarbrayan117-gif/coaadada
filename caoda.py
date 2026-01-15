12d775c87195564438f47869da9941731228ba23
alcivarbrayan117-gif <alcivarbrayan117@gmail.com>
2026-01-15 02:02:48 -0500

    Update and rename caoda to caoda.py

---
+++ caoda.py
@@ -0,0 +1,45 @@
+def funcion_mala(a,b,c):
+    if a>0:
+        if b>0:
+            if c>0:
+                if a>b:
+                    if b>c:
+                        if a+b+c>10:
+                            print("Complejidad alta")
+
+    x=0
+    x=0
+
+    y = None
+    if y == None:
+        print("Comparación incorrecta con None")
+
+    for i in range(5):
+            print(i)
+
+    return
+
+
+def otra_funcion():
+    password = "123456"
+    print("Password:", password)
+
+    try:
+        z = 10 / 0
+    except:
+        pass
+
+
+def main():
+    datos=[1,2,3]
+    funcion_mala(1,2,3)
+    otra_funcion()
+    if True:
+        print("ok")
+        print("ok")
+        print("ok")
+        print("ok")
+        print("ok")
+
+
+main()
